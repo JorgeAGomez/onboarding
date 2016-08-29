e7eb0ec7593f0bfdd568dadef7c6acb9c4c516f1
Jorge <jorge_gomez12@hotmail.com>
2016-08-28 22:35:50 -0600

    Finished modifying UI buttons and labels for the UITableViewControllers

--- onboarding/onboarding/SubmitScreenViewController.swift
+++ onboarding/onboarding/SubmitScreenViewController.swift
@@ -10,17 +10,24 @@ import UIKit
 import MessageUI
 class SubmitScreenViewController: UIViewController, MFMailComposeViewControllerDelegate {
 
+  @IBOutlet weak var submitButton: UIButton!
+  
     override func viewDidLoad() {
       super.viewDidLoad()
         pageControl.hidesForSinglePage = true
+        submitButton.layer.borderColor = submitButton.tintColor.CGColor
+        self.navigationItem.hidesBackButton = true
       
-        }
+    }
 
     override func didReceiveMemoryWarning() {
       super.didReceiveMemoryWarning()
       // Dispose of any resources that can be recreated.
     }
   
+  @IBAction func submitButton(sender: AnyObject) {
+    self.performSegueWithIdentifier("goBack", sender: self)
+  }
   
     
 
